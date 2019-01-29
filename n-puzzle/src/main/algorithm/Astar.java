b24df142b992fd12651caf8c284455c13cb56ff8
Eugene Garagulya <EugeneGxUA>
2019-01-29 11:23:56 +0200

    wrong way

--- n-puzzle/src/main/algorithm/Astar.java
+++ n-puzzle/src/main/algorithm/Astar.java
@@ -54,7 +54,6 @@ public class Astar <TState extends State, TRules extends Rules<TState>> {
                             open.add(tempQ.poll());
                         }
                     }
-                    neighbor.setG(g);
                     System.out.println(neighbor.getG() + "G \n"+ neighbor.getH() + " H");
                     open.add(neighbor);
                     isGBetter = true;
